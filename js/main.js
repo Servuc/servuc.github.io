dea6362b842f11abbae726e32c0d89d952956e79
Thomas Servuc <thomas.selle@orange.fr>
2015-11-27 02:56:55 +0100

    C'est bon

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by thomas on 27/11/15.
+ */
+
+$(document).foundation();
+
+$("a[data]").click(function(){$('html, body').animate({scrollTop: $("[data-scroll='" + $(this).attr("data") + "']").offset().top}, 2000);})
+$(".fa-arrow-circle-o-up").click(function(){$('html, body').animate({scrollTop: 0}, 2000);});
+$(document).bind('scroll', function()
+{
+    var myScrollVal = 300;
+    var wh = $(window).height();
+    var dt = $(document).scrollTop() + $(window).height() - myScrollVal / 2;
+    if(wh > dt)
+        $(".fa-arrow-circle-o-up").css("opacity", "0");
+    else if(wh + myScrollVal < dt)
+        $(".fa-arrow-circle-o-up").css("opacity", "1");
+    else
+        $(".fa-arrow-circle-o-up").css("opacity", "" + ((dt - wh) * 1 / myScrollVal).toFixed(2));
+});
